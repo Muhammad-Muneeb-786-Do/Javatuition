075231b583ecd5485adfd8ff800a9db04b0b5538
Muneeb <muhammadmuneeb3042@gmail.com>
2024-12-01 02:33:29 +0500

    lecture 4

---
+++ lecture4/README.md
@@ -0,0 +1,10 @@
+# Literals in java
+
+Any value that we assign to any variable is termed as literal and liternal can be any value.
+
+We can assign binary or hex value to an integer and it will automatically convert that value to integer.
+
+we have to place 0b before a binary number.
+we have to place 0x before a hexa number.
+
+Plese check code for implementation.
---
+++ lecture4/src/literals.java
@@ -0,0 +1,25 @@
+public class literals{
+    public static void main(String[] args) {
+        //literals
+
+        int binarynumber = 0b101011; //binary number
+        int hexnumber = 0x12F; //hexadecimal number
+
+        double dabal = 90; // it will autotically convert integer to doubel.
+        float flat = 12.0f; //to assign floating point number we have to explicity mention them.
+
+        char character = 'a';
+        character++; // it will make the character b.
+
+        System.out.println(binarynumber);
+        System.out.println(hexnumber);
+        System.out.println(dabal);
+        System.out.println(flat);
+        System.out.println(character);
+
+
+
+
+        
+    }
+}
